fb56d921a7dc483b302fa73159287f055a86a82f
HugoLambrinidis <hlambrinidis@gmail.com>
2016-02-08 16:09:34 +0100

    addController Folder / add FrontController

---
+++ Controller/FrontController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controller;
+
+Class FrontController {
+
+    public function __construct()
+    {
+
+        $conf = json_decode(file_get_contents("conf.json"));
+        $current_request = $_SERVER['REQUEST_URI'];
+        $request = explode($conf->url, $current_request);
+
+        // instantiate router with $request[1] param
+    }
+}
